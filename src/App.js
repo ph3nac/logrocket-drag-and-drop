b6f016edb2c0d6ded1bfbe4b831ed9fa2be3221a
ph3 <100564304+ph3nac@users.noreply.github.com>
2022-11-24 17:41:40 +0900

    list drag and drop change

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import { useCallback, useState } from "react";
 import Dropzone from "./Dropzone";
 import cuid from "cuid";
 import ImageList from "./ImageList";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import update from "immutability-helper";
 
 function App() {
   const [images, setImages] = useState([]);
+  const moveImage = (dragIndex, hoverIndex) => {
+    const draggableImage = images[dragIndex];
+    setImages(
+      update(images, {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, draggableImage],
+        ],
+      })
+    );
+  };
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.map((file) => {
       const reader = new FileReader();
@@ -24,7 +38,9 @@ function App() {
     <main className="App">
       <h1 className="text-center">Drag and Drop Example</h1>
       <Dropzone onDrop={onDrop} accept={"image/*"} />
-      <ImageList images={images} />
+      <DndProvider backend={HTML5Backend}>
+        <ImageList images={images} moveImage={moveImage} />
+      </DndProvider>
     </main>
   );
 }
--- src/ImageList.js
+++ src/ImageList.js
@@ -1,14 +1,59 @@
-const Image = ({ image }) => {
+import { useRef } from "react";
+import { useDrop, useDrag } from "react-dnd";
+const type = "Image";
+
+const Image = ({ image, index, moveImage }) => {
+  const ref = useRef(null); // Initialize the reference
+
+  const [, drop] = useDrop({
+    accept: type,
+    hover(item) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      moveImage(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: type,
+    item: { id: image.id, index },
+    collect: (monitor) => {
+      return {
+        isDragging: monitor.isDragging(),
+      };
+    },
+  }));
+
+  drag(drop(ref));
+
   return (
-    <div className="file-item">
+    <div
+      ref={ref}
+      style={{ opacity: isDragging ? 0 : 1 }}
+      className="file-item"
+    >
       <img alt={`img - ${image.id}`} src={image.src} className="file-img" />
     </div>
   );
 };
 
-const ImageList = ({ images }) => {
+const ImageList = ({ images, moveImage }) => {
   const renderImage = (image, index) => {
-    return <Image image={image} key={`${image.id}-image`} />;
+    return image ? (
+      <Image
+        image={image}
+        index={index}
+        key={`${image.id}-image`}
+        moveImage={moveImage}
+      />
+    ) : null;
   };
   return <section className="file-list">{images.map(renderImage)}</section>;
 };
